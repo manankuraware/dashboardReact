d21fd751b931cbe6901bd3e7d8a7b1acca20b989
manankuraware <manankuraware2017@gmail.com>
2024-01-23 20:02:36 +0530

    sidebar done

---
+++ dashboard-client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return <>
+    <Router>
+      <Sidebar />
+    </Router>
+  </>;
+}
+
+export default App;
